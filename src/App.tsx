82615c15a1968e4f7ae5ca75ec5c3f3638fcb77e
Haocheng Yang <haocheng@Haochengs-aMacBook-Air.local>
2024-12-21 22:59:00 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import TrumpPolicies from './pages/TrumpPolicies';
 import Inauguration from './pages/Inauguration';
@@ -11,14 +11,25 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Home route with trailing slash */}
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutTrump />} />
-        <Route path="/policies" element={<TrumpPolicies />} />
-        <Route path="/speeches" element={<TrumpSpeeches />} />
-        <Route path="/inauguration" element={<Inauguration />} />
-        <Route path="/quotes" element={<TrumpQuotes />} />
+        
+        {/* Content routes with trailing slashes */}
+        <Route path="/about/" element={<AboutTrump />} />
+        <Route path="/policies/" element={<TrumpPolicies />} />
+        <Route path="/speeches/" element={<TrumpSpeeches />} />
+        <Route path="/inauguration/" element={<Inauguration />} />
+        <Route path="/quotes/" element={<TrumpQuotes />} />
+        
+        {/* Redirect routes without trailing slash to trailing slash */}
+        <Route path="/about" element={<Navigate to="/about/" replace />} />
+        <Route path="/policies" element={<Navigate to="/policies/" replace />} />
+        <Route path="/speeches" element={<Navigate to="/speeches/" replace />} />
+        <Route path="/inauguration" element={<Navigate to="/inauguration/" replace />} />
+        <Route path="/quotes" element={<Navigate to="/quotes/" replace />} />
+        
         {/* Catch all route - redirect to home */}
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
